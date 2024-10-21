4248ffbcb439411757b521e4ff648881f5c3f4f6
tyu00 <g.s.u.toshov2002@yandex.com>
2024-10-21 20:19:42 +0500

    #5

--- NewsProject/settings.py
+++ NewsProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'bookstore',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'NewsProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
+from .filters import BookFilter
 
-# Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    book_filter = BookFilter(request.GET, queryset=books)
+    return render(request, 'book_list.html', {'filter': book_filter})
